25a11bb18efb1e757fb955f9573a400e6a6f4276
Chitiwat Phajan <chitiwat2547@gmail.com>
2025-12-12 06:15:26 +0700

    prisma

--- frontend/lib/db.ts
+++ frontend/lib/db.ts
@@ -1,13 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+// lib/db.ts
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+  
+const dbUrl = new URL(process.env.DATABASE_URL!);
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const adapter = new PrismaMariaDb({
+  host: dbUrl.hostname,
+  port: Number(dbUrl.port || "3306"),
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.slice(1), 
+  connectionLimit: 5,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter }); 
+};
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"],
-  });
+  globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
