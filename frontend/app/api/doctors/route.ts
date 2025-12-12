44db08d6b3c50ba0b8a1c8718e84b650c6bbffea
MiniMammos <paramee.sae@ku.th>
2025-12-12 20:29:26 +0700

    nothing

--- frontend/app/api/doctors/route.ts
+++ frontend/app/api/doctors/route.ts
@@ -1,13 +1,39 @@
 import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const doctors = await prisma.doctor.findMany({
-    include: {
-      department: true,
-      specialty: true,
-    },
-    orderBy: { doctor_id: "asc" },
-  });
-  return NextResponse.json(doctors);
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  if (body.action === "getAppointments") {
+    // query appoint upto doctor_id
+    const doctorId = parseInt(body.id);
+    const appointments = await prisma.appointment.findMany({
+      where: { doctor_id: doctorId },
+      include: { doctor: { select: { name: true } } },
+      orderBy: { appointment_id: "asc" },
+    });
+    return NextResponse.json(appointments);
+  } else if (body.action === "createAppointment") {
+    //create appointment
+    const appointment = await prisma.appointment.create({
+      data: {
+        patient_id: body.patient_id,
+        doctor_id: body.doctor_id,
+        date: new Date(body.date),
+        time: body.time,
+        status: body.status,
+      },
+    });
+    return NextResponse.json(appointment);
+  }
+
+  return NextResponse.json({ error: "Invalid action" }, { status: 400 });
 }
+
+
+
+
+
+
+
+
